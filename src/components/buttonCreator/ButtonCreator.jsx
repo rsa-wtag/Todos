0e174a059fe362e5449040b9e1e8e7a00b812e78
Rumman Sadik <rumman.sadik@welldev.io>
2023-05-22 12:38:10 +0600

    Create Button component Delete

--- src/components/buttonCreator/ButtonCreator.jsx
+++ src/components/buttonCreator/ButtonCreator.jsx
@@ -1,10 +1,16 @@
 import PropTypes from "prop-types";
 import "src/components/buttonCreator/ButtonCreator.component.scss";
 
-export const ButtonCreator = ({ icon, altText }) => {
+export const ButtonCreator = ({
+  onButtonClick,
+  buttonText,
+  icon,
+  altText,
+  buttonClass,
+}) => {
   return (
-    <button className="button">
-      <img src={icon} alt={altText} />
+    <button className={"button " + buttonClass} onClick={onButtonClick}>
+      <img src={icon} alt={altText} /> {buttonText}
     </button>
   );
 };
--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -2,8 +2,9 @@ import { useState } from "react";
 import { useDispatch } from "react-redux";
 import CreateTasks from "src/components/tasks/CreateTasks";
 import TaskItems from "src/components/tasks/TaskList";
-import CreateButton from "src/components/tasks/CreateButton";
 import { addTasks } from "src/store/actions/taskActions";
+import { ALT_ADD_TEXT, ICON_ADD } from "src/utils/constants/constants";
+import { ButtonCreator } from "src/components/buttonCreator/ButtonCreator";
 import "src/components/content/Content.component.scss";
 
 const Content = () => {
@@ -15,10 +16,20 @@ const Content = () => {
     dispatch(addTasks(sanitizedValue));
   }
 
+  function toggleShow() {
+    setShowTask(!showTask);
+  }
+
   return (
     <div className="content">
       <h1>Add Tasks</h1>
-      <CreateButton showTask={showTask} onShow={setShowTask} />
+      <ButtonCreator
+        onButtonClick={toggleShow}
+        buttonText={"create"}
+        icon={ICON_ADD}
+        altText={ALT_ADD_TEXT}
+        buttonClass={"content__button__create"}
+      />
       {showTask && <CreateTasks onAddTask={onAddTask} />}
       <TaskItems />
     </div>
