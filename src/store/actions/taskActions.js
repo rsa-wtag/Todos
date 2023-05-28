f52e52c18c97c3339b63cff83bd7b2331a14eb4a
Rumman Sadik <rumman.sadik@welldev.io>
2023-05-28 17:28:45 +0600

    remove return statements from actions

--- src/store/actions/taskActions.js
+++ src/store/actions/taskActions.js
@@ -1,21 +1,17 @@
 import { ADD_TASK, REMOVE_TASK } from "src/store";
 import { createDate } from "src/utils/helpers/createDate";
 
-export const addTasks = (title) => {
-  return {
-    type: ADD_TASK,
-    payload: {
-      id: Date.now().toString(),
-      isCompleted: false,
-      title: title,
-      createdAt: createDate(),
-    },
-  };
-};
+export const addTasks = (title) => ({
+  type: ADD_TASK,
+  payload: {
+    id: Date.now().toString(),
+    isCompleted: false,
+    title: title,
+    createdAt: createDate(),
+  },
+});
 
-export const deleteTask = (taskId) => {
-  return {
-    type: REMOVE_TASK,
-    payload: taskId,
-  };
-};
+export const deleteTask = (taskId) => ({
+  type: REMOVE_TASK,
+  payload: taskId,
+});
